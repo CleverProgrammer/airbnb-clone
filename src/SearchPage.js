4f0041efab11f6b3d3e479123ec1d6ed2271ea86
Tanish Dahiya <48643197+TanishDahiya@users.noreply.github.com>
2020-10-02 00:50:44 +0530

    Update SearchPage.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -19,7 +19,7 @@ function SearchPage() {
                 img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
                 location="Private room in center of London"
                 title="Stay at this spacious Edwardian House"
-                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine"
                 star={4.73}
                 price="£30 / night"
                 total="£117 total"
@@ -29,7 +29,7 @@ function SearchPage() {
                 img="https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg"
                 location="Private room in center of London"
                 title="Independant luxury studio apartment"
-                description="2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen"
+                description="2 guest · 3 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen"
                 star={4.3}
                 price="£40 / night"
                 total="£157 total"
@@ -48,7 +48,7 @@ function SearchPage() {
                 img="https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI"
                 location="Private room in center of London"
                 title="30 mins to Oxford Street, Excel London"
-                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine"
                 star={4.1}
                 price="£55 / night"
                 total="£320 total"
@@ -57,7 +57,7 @@ function SearchPage() {
                 img="https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg"
                 location="Private room in center of London"
                 title="Spacious Peaceful Modern Bedroom"
-                description="3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning"
+                description="3 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Free parking · Dry Cleaning"
                 star={5.0}
                 price="£60 / night"
                 total="£450 total"
@@ -66,7 +66,7 @@ function SearchPage() {
                 img="https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937"
                 location="Private room in center of London"
                 title="The Blue Room In London"
-                description="2 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Washing Machine"
+                description="2 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Washing Machine"
                 star={4.23}
                 price="£65 / night"
                 total="£480 total"
@@ -75,7 +75,7 @@ function SearchPage() {
                 img="https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp"
                 location="Private room in center of London"
                 title="5 Star Luxury Apartment"
-                description="3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                description="3 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine"
                 star={3.85}
                 price="£90 / night"
                 total="£650 total"
